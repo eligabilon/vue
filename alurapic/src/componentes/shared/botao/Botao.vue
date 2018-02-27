961d34e34f468e92d8cd0a2b063898201639ca16
eli.fernandes <eligabilon@gmail.com>
2018-02-27 17:41:45 -0400

    primeiro commit

---
+++ alurapic/src/componentes/shared/botao/Botao.vue
@@ -0,0 +1,52 @@
+<template>
+	<button @click="disparaAcao()" class="botao botao-perigo" :type="tipo">{{ rotulo }}</button>
+</template>
+
+<script type="text/javascript">
+	export default{
+		props: {
+			
+			tipo: {
+				required: true,
+				type: String	
+			},
+			
+			rotulo: {
+				required:true,
+				type: String
+			},
+			
+			confirmacao: Boolean,
+			estilo: String
+		},
+
+		methods:{
+			disparaAcao(){
+				if(confirm('Confirma operação?')){
+					this.$emit('botaoAtivado', new Date());
+				}
+			}
+		}
+	}
+	
+</script>
+
+<style type="text/css">
+	.botao{
+		display: inline-block;
+		padding: 10px;
+		border-radius: 3px;
+		margin: 10px;
+		font-size: 1.2em;
+	}
+
+	.botao-perigo{
+		background: firebrick;
+		color: white;		
+	}
+
+	.botao-padrao{
+		background: darkcyan;
+		color: white;
+	}
+</style>
